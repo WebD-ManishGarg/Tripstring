e96ad6d498a1e38710473b59f9718dfa28de8cc6
Manish <gargmanish097@gmail.com>
2018-09-03 22:32:55 +0530

    first

---
+++ app.js
@@ -0,0 +1,45 @@
+var express=require("express"),
+    app=express();
+
+app.set("view engine","ejs");
+app.use(express.static(__dirname + '/public'));
+
+
+//routes
+
+//landing routes
+
+app.get("/",function(req,res){
+  res.render("landing");
+});
+
+//index route
+
+app.get("/tripstring",function(req,res){
+      res.render("index");
+});
+
+//offer pages
+app.get("/shimla",function(req,res){
+  res.render("package-tour/shimla1");
+})
+//form show pages
+app.get("/form",function(req,res){
+   res.render("form");
+});app.get("/form_wedding",function(req,res){
+   res.render("form1");
+});
+app.get("/form_mice",function(req,res){
+   res.render("mice");
+});
+
+app.get("/about",function(req,res){
+   res.render("about");
+});
+
+
+
+
+app.listen(3000,function(){
+  console.log("server is started .......")
+});
